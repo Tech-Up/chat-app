fbc20a8c6076275a4c870b8d1d5d95f4568c1327
Ariel Silva Jr <ariel@mycape.io>
2016-06-18 15:51:05 +0800

    Update register activity

--- app/src/main/java/io/techup/app/chatapp/RegisterActivity.java
+++ app/src/main/java/io/techup/app/chatapp/RegisterActivity.java
@@ -3,6 +3,7 @@ package io.techup.app.chatapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -12,66 +13,68 @@ import org.apache.commons.validator.routines.EmailValidator;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        initView();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_register);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    initView();
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                break;
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        onBackPressed();
+        break;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void initView() {
+
+    Button btnSubmit = (Button) findViewById(R.id.btn_submit);
+
+    final EditText edtEmail = (EditText) findViewById(R.id.edt_email);
+    final EditText edtPassword = (EditText) findViewById(R.id.edt_password);
+
+    if (btnSubmit != null) {
+      btnSubmit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+          if (edtEmail != null) {
+            String email = edtEmail.getText().toString();
+            if (!isValidEmail(email)) {
+              edtEmail.setError("Invalid email");
+            }
+          }
+
+          if (edtPassword != null) {
+            String password = edtPassword.getText().toString();
+            if (password.length() == 0) {
+              edtPassword.setError("Please enter your password.");
+              return;
+            }
+            if (password.length() < 6) {
+              edtPassword.setError("Please enter at least 6 character.");
+              return;
+            }
+          }
+
+          Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+          intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+          startActivity(intent);
+          finish();
         }
-        return super.onOptionsItemSelected(item);
+      });
     }
+  }
 
-    private void initView() {
-
-        Button btnSubmit = (Button) findViewById(R.id.btn_submit);
-
-        final EditText edtEmail = (EditText) findViewById(R.id.edt_email);
-        final EditText edtPassword = (EditText) findViewById(R.id.edt_password);
-
-        if (btnSubmit != null) {
-            btnSubmit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    if (edtEmail != null) {
-                        String email = edtEmail.getText().toString();
-                        if (!isValidEmail(email)) {
-                            edtEmail.setError("Invalid email");
-                        }
-                    }
-
-                    if (edtPassword != null) {
-                        String password = edtPassword.getText().toString();
-                        if (password.length() == 0) {
-                            edtPassword.setError("Please enter your password.");
-                            return;
-                        }
-                        if (password.length() < 6) {
-                            edtPassword.setError("Please enter at least 6 character.");
-                            return;
-                        }
-                    }
-
-                    Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    finish();
-                }
-            });
-        }
-    }
-
-    private boolean isValidEmail(String email) {
-        return !email.isEmpty() && EmailValidator.getInstance().isValid(email);
-    }
+  private boolean isValidEmail(String email) {
+    return !email.isEmpty() && EmailValidator.getInstance().isValid(email);
+  }
 
 }
