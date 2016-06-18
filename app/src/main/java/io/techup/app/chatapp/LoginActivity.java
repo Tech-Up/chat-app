6fce843a78e5933c4ca2d3c59945e3f3deb5ed27
Ariel Silva Jr <ariel@mycape.io>
2016-06-18 15:51:58 +0800

    update login activity

--- app/src/main/java/io/techup/app/chatapp/LoginActivity.java
+++ app/src/main/java/io/techup/app/chatapp/LoginActivity.java
@@ -6,69 +6,81 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        initView();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    initView();
+  }
 
-    private void initView() {
+  private void initView() {
 
-        final Button btnLogin = (Button) findViewById(R.id.btn_login);
-        final EditText edtEmail = (EditText) findViewById(R.id.edt_email);
-        final EditText edtPassword = (EditText) findViewById(R.id.edt_password);
+    final Button btnLogin = (Button) findViewById(R.id.btn_login);
+    final EditText edtEmail = (EditText) findViewById(R.id.edt_email);
+    final EditText edtPassword = (EditText) findViewById(R.id.edt_password);
 
-        if (btnLogin != null) {
-            btnLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+    if (btnLogin != null) {
+      btnLogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-                    if (edtEmail != null) {
-                        String email = edtEmail.getText().toString();
-                        if (!isValidEmail(email)) {
-                            edtEmail.setError("Invalid email");
-                        }
-                    }
+          if (edtEmail != null) {
+            String email = edtEmail.getText().toString();
+            if (!isValidEmail(email)) {
+              edtEmail.setError("Invalid email");
+            }
+          }
 
-                    if (edtPassword != null) {
-                        String password = edtPassword.getText().toString();
-                        if (password.length() == 0) {
-                            edtPassword.setError("Please enter your password.");
-                            return;
-                        }
-                        if (password.length() < 6) {
-                            edtPassword.setError("Please enter at least 6 character.");
-                            return;
-                        }
-                    }
+          if (edtPassword != null) {
+            String password = edtPassword.getText().toString();
+            if (password.length() == 0) {
+              edtPassword.setError("Please enter your password.");
+              return;
+            }
+            if (password.length() < 6) {
+              edtPassword.setError("Please enter at least 6 character.");
+              return;
+            }
+          }
 
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    finish();
+          Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+          startActivity(intent);
+          finish();
 
-                }
-            });
         }
+      });
+    }
 
-        Button btnRegister = (Button) findViewById(R.id.btn_register);
-        if (btnRegister != null) {
-            btnRegister.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                    startActivity(intent);
-                }
-            });
+    Button btnRegister = (Button) findViewById(R.id.btn_register);
+    if (btnRegister != null) {
+      btnRegister.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+          startActivity(intent);
         }
+      });
     }
 
-    private boolean isValidEmail(String email) {
-        return !email.isEmpty() && EmailValidator.getInstance().isValid(email);
+    TextView textView = (TextView) findViewById(R.id.tv_forgot_password);
+    if (textView != null) {
+      textView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+          startActivity(intent);
+        }
+      });
     }
+  }
+
+  private boolean isValidEmail(String email) {
+    return !email.isEmpty() && EmailValidator.getInstance().isValid(email);
+  }
 }
