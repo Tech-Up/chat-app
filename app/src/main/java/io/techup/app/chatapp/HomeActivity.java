4965fc4316c7fee7cd8db9fb954cdb40229a5274
Ariel Silva Jr <ariel@mycape.io>
2016-06-18 17:15:31 +0800

    Initial commit for write new message screen

--- app/src/main/java/io/techup/app/chatapp/HomeActivity.java
+++ app/src/main/java/io/techup/app/chatapp/HomeActivity.java
@@ -3,7 +3,6 @@ package io.techup.app.chatapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -29,13 +28,15 @@ public class HomeActivity extends AppCompatActivity {
     setSupportActionBar(toolbar);
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
-      }
-    });
+    if (fab != null) {
+      fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          Intent intent = new Intent(HomeActivity.this, NewMessageActivity.class);
+          startActivity(intent);
+        }
+      });
+    }
 
     initView();
   }
