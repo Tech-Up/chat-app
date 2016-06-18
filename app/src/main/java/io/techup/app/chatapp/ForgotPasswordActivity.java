daf0d969370eb8b1ece4bbcf9659b01727222c5e
Ariel Silva Jr <ceosilvajr@gmail.com>
2016-06-18 15:48:15 +0800

    Added forgot password activity

---
+++ app/src/main/java/io/techup/app/chatapp/ForgotPasswordActivity.java
@@ -0,0 +1,30 @@
+package io.techup.app.chatapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_forgot_password);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        onBackPressed();
+        break;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+
+}
