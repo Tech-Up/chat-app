7c2059d54272e8ab204fdd7ee94dc42dcd0408e8
Ariel Silva Jr <ariel@mycape.io>
2016-06-27 21:12:04 +0800

    implemented dialogs

--- app/src/main/java/io/techup/app/chatapp/MyProfileActivity.java
+++ app/src/main/java/io/techup/app/chatapp/MyProfileActivity.java
@@ -1,9 +1,12 @@
 package io.techup.app.chatapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MyProfileActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class MyProfileActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    initView();
   }
 
   @Override
@@ -24,6 +28,41 @@ public class MyProfileActivity extends AppCompatActivity {
         break;
     }
     return super.onOptionsItemSelected(item);
+  }
+
+  private void initView() {
+    ImageButton btnEditFirstName = (ImageButton) findViewById(R.id.btn_edit_first_name);
+    if (btnEditFirstName != null) {
+      btnEditFirstName.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          popupEditFirstName();
+        }
+      });
+    }
+    ImageButton btnEditLastName = (ImageButton) findViewById(R.id.btn_edit_last_name);
+    if (btnEditLastName != null) {
+      btnEditLastName.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          popUpEditLastName();
+        }
+      });
+    }
+  }
+
+  private void popupEditFirstName() {
+    final Dialog dialog = new Dialog(this);
+    dialog.setContentView(R.layout.dialog_edit_first_name);
+    dialog.setTitle("Edit first name");
+    dialog.show();
+  }
+
+  private void popUpEditLastName() {
+    final Dialog dialog = new Dialog(this);
+    dialog.setContentView(R.layout.dialog_edit_last_name);
+    dialog.setTitle("Edit first name");
+    dialog.show();
   }
 
 }
